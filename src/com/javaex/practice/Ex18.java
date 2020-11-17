1c125cafbb0fd0ee7bf0e9d9d687d13e81eeacc9
kimsy <duddl2102@naver.com>
2020-11-17 16:38:23 +0900

    반복문 과제

--- src/com/javaex/practice/Ex18.java
+++ src/com/javaex/practice/Ex18.java
@@ -6,6 +6,7 @@ public class Ex18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("숫자를 입력하세요 : ");
@@ -17,13 +18,19 @@ public class Ex18 {
 			}
 			System.out.println("");
 		}
-		for(int i=1; i<num; i++) {
-			for(int j=1; j<=i; j++) {
+		for(int i=2; i<=num; i++) {
+			for(int j=i; j>0; j--) {
 				System.out.print("*");
 			}
-			System.out.print("*");
 			System.out.println("");
 		}
+//		for(int i=1; i<num; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.print("*");
+//			System.out.println("");
+//		}
 		sc.close();
 	}
 
